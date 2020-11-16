594bcf7886955eb8c4186662753e3fb384772a09
natalianowak1 <natalia.nowak1@gmail.com>
2020-11-16 20:23:54 +0100

    create and route pages

--- src/layouts/Page.js
+++ src/layouts/Page.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+import HomePage from '../pages/HomePage';
+import ProductPage from '../pages/ProductPage';
+import ContactPage from '../pages/ContactPage';
+import AdminPage from '../pages/AdminPage';
+import ErrorPage from '../pages/ErrorPage';
+
+import '../styles/Page.css';
 
 const Page = () => {
     return (
-        <div>Page</div>
+        <React.Fragment>
+            <Switch>
+                <Route path="/" exact component={HomePage}></Route>
+                <Route path="/products" component={ProductPage}></Route>
+                <Route path="/contact" component={ContactPage}></Route>
+                <Route path="/admin" component={AdminPage}></Route>
+                <Route component={ErrorPage}></Route>
+            </Switch>
+        </React.Fragment>
     );
 }
 
