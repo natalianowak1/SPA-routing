b5f53426faa13340d07489696606af3c3998d07c
natalianowak1 <natalia.nowak1@gmail.com>
2020-11-16 20:07:10 +0100

    create and style header

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -1,8 +1,35 @@
 import React from 'react';
+import { matchPath, Route, Switch } from 'react-router';
+
+import header1 from '../images/apple.jpg';
+import header2 from '../images/bmw.jpeg';
+import header3 from '../images/ferrari.jpg';
+import header4 from '../images/merc.jpg';
+
+import '../styles/Header.css';
+
 
 const Header = () => {
     return (
-        <div>Header</div>
+        <>
+            <Switch>
+                <Route path="/" exact render={() => (
+                    <img src={header4} alt="merc" />
+                )} ></Route>
+                <Route path="/products" render={() => (
+                    <img src={header1} alt="apple" />
+                )} ></Route>
+                <Route path="/contact" render={() => (
+                    <img src={header2} alt="bmw" />
+                )} ></Route>
+                <Route path="/admin" render={() => (
+                    <img src={header3} alt="ferrari" />
+                )} ></Route>
+                <Route render={() => (
+                    <img src={header4} alt="merc" />
+                )} ></Route>
+            </Switch>
+        </>
     );
 }
 
