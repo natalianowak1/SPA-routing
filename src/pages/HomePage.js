abfe3a88147d811ee3e0c2113f02246bade476d7
natalianowak1 <natalia.nowak1@gmail.com>
2020-11-16 20:39:31 +0100

    create and style Article. use it in HomePage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,8 +1,50 @@
 import React from 'react';
 
+import Article from '../components/Article';
+
+const articles = [
+    {
+        id: 1,
+        title: "tytuł pierwszego artykułu",
+        author: "autor pierwszy",
+        text: "pierwszy Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur modi fuga veritatis dignissimos architecto similique harum eligendi placeat veniam quam, odit optio ducimus corporis nisi distinctio earum magnam laudantium fugit?"
+    },
+    {
+        id: 2,
+        title: "tytuł drugiego artykułu",
+        author: "autor drugi",
+        text: "drugi Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur modi fuga veritatis dignissimos architecto similique harum eligendi placeat veniam quam, odit optio ducimus corporis nisi distinctio earum magnam laudantium fugit?"
+    },
+    {
+        id: 3,
+        title: "tytuł trzeciego artykułu",
+        author: "autor trzeci",
+        text: "trzeciLorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur modi fuga veritatis dignissimos architecto similique harum eligendi placeat veniam quam, odit optio ducimus corporis nisi distinctio earum magnam laudantium fugit?"
+    },
+    {
+        id: 4,
+        title: "tytuł czwartego artykułu",
+        author: "autor czwarty",
+        text: "czwartyLorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur modi fuga veritatis dignissimos architecto similique harum eligendi placeat veniam quam, odit optio ducimus corporis nisi distinctio earum magnam laudantium fugit?"
+    },
+    {
+        id: 5,
+        title: "tytuł piątego artykułu",
+        author: "autor piąty",
+        text: "5555Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur modi fuga veritatis dignissimos architecto similique harum eligendi placeat veniam quam, odit optio ducimus corporis nisi distinctio earum magnam laudantium fugit?"
+    }
+]
+
 const HomePage = () => {
+
+    const artList = articles.map(article => (
+        <Article key={article.id} {...article} />
+    ))
+
     return (
-        <div>Strona Startowa</div>
+        <div className="Home">
+            {artList}
+        </div>
     );
 }
 
