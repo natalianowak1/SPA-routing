efad2f0d9f46d89242668c87f9bb60ef2d5d0449
natalianowak1 <natalia.nowak1@gmail.com>
2020-11-16 21:22:28 +0100

    create and style ContactPage

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
+import '../styles/ContactPage.css';
+import { Prompt } from 'react-router-dom';
 
-const ContactPage = () => {
-    return (
-        <div>
-            ContactPage
-        </div>
-    );
+class ContactPage extends Component {
+    state = {
+        value: ""
+    }
+
+    handleChange = e => {
+        this.setState({
+            value: e.target.value
+        })
+    }
+
+    handleSubmit = e => {
+        e.preventDefault();
+        this.setState({
+            value: ""
+        })
+    }
+
+    render() {
+        return (
+            <div className="contact">
+                <form onSubmit={this.handleSubmit}>
+                    <h3>Napisz do nas</h3>
+                    <textarea value={this.state.value} onChange={this.handleChange} placeholder="Wpisz wiadomość..."></textarea>
+                    <button>Wyślij</button>
+                </form>
+                <Prompt
+                    when={this.state.value}
+                    message="Masz niewypełniony formularz. Czy na pewno chcesz opuścić tę stronę?"
+                />
+            </div>
+        );
+    }
 }
 
 export default ContactPage;
