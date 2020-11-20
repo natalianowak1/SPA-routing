7fd6da2dc29f9cabc76de187246207723e8f3327
natalianowak1 <natalia.nowak1@gmail.com>
2020-11-20 17:53:06 +0100

    create and style product list page

--- src/layouts/Page.js
+++ src/layouts/Page.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import HomePage from '../pages/HomePage';
+import ProductListPage from '../pages/ProductListPage';
 import ProductPage from '../pages/ProductPage';
 import ContactPage from '../pages/ContactPage';
 import AdminPage from '../pages/AdminPage';
@@ -15,7 +16,7 @@ const Page = () => {
         <React.Fragment>
             <Switch>
                 <Route path="/" exact component={HomePage}></Route>
-                <Route path="/products" component={ProductPage}></Route>
+                <Route path="/products" component={ProductListPage}></Route>
                 <Route path="/contact" component={ContactPage}></Route>
                 <Route path="/admin" component={AdminPage}></Route>
                 <Route path="/login" component={LoginPage}></Route>
